539dd14d82758102567bbd2e5a4571860e48221c
sg673 <sg673@student.le.ac.uk>
2026-02-24 12:38:06 +0000

    fix: renamed import

--- Producer/data_loader.py
+++ Producer/data_loader.py
@@ -1,6 +1,6 @@
 import json
 from typing import Any, Dict, Optional
-from Producer.types import Merchant, User
+from Producer.custom_types import Merchant, User
 from user_profiles import generate_profiles
 
 
